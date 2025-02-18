f5f7376960f5866255dffa6b409187707cdd54a0
Aleksandr Babayan <babayanaleksandr80@gmail.com>
2025-02-18 19:27:17 +0400

    add docker for postgres

--- src/types/env.types.d.ts
+++ src/types/env.types.d.ts
@@ -3,5 +3,10 @@ declare namespace NodeJS {
         SERVER_PORT: number;
         SERVER_PROTOCOL: string;
         SERVER_HOST: string;
+        POSTGRES_USER: string;
+        POSTGRES_PASSWORD: string;
+        POSTGRES_HOST: string;
+        POSTGRES_PORT: number;
+        POSTGRES_DB: string;
     }
 }
